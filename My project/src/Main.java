8b5b78ae264840a6db3115faf078d131256d2dbd
Sergei Borisenko <seregarzn87@yandex.ru>
2023-07-14 10:40:26 +0300

    First commit

---
+++ My project/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void Main(String[] args) {
+        double rubles = 35000.0;
+        double rateUSD = 78.5;
+        double dollars = rubles / rateUSD;
+
+        System.out.println(dollars);
+    }
+    }
+
